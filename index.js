742c2f08b9243c9f7a4670dff6982d88f0667a04
SheillaO <sheilla.alusa@strathmore.edu>
2026-04-18 21:32:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const declineBtn = document.getElementById("decline-btn");
 const modalChoiceBtns = document.getElementById("modal-choice-btns");
 const darkPatternWarning = document.getElementById("dark-pattern-warning");
 
+let darkPatternTriggers = 0;
+let sessionStartTime = Date.now();
